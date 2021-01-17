b2c5c3f880424a3b960dc600a4e28a8164b38d33
agrah355 <74407072+agrah355@users.noreply.github.com>
2021-01-17 13:50:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var input=[];
+function getPeragraph(){
+    for(var i=0;i<input;i++){
+        p.push(document.getElementById("peragraph_1"+i).velue);
+        p.join(". ");
+    }
+    console.log(input);
+    document.getElementById("get_peragraph").velue;
+}
